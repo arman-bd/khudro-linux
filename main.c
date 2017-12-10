e7b2f011f443022c28067572189762d6f8609175
Arman Hossain <arman@learnfie.com>
2017-12-11 01:16:11 +0600

    Major changes

--- main.c
+++ main.c
@@ -10,11 +10,7 @@ GitHub: https://github.com/arman-bd/khudro-linux
 LinkedIn: https://www.linkedin.com/in/armanhossain/
 */
 
-char __ServerName[128] = "Khudro";
-char __ServerVersion[16] = "0.1.1";
-char __ServerOS[32] = "Linux";
 
-int GlobalSock = -1;
 
 #define FAIL    -1
 
@@ -46,10 +42,23 @@ int GlobalSock = -1;
 #include "lib/map_lib/map_lib.h"
 #include "lib/map_lib/map_lib.c"
 
+char __ServerName[128] = "Khudro";
+char __ServerVersion[16] = "0.1.1";
+char __ServerOS[32] = "Linux";
+int GlobalSock = -1;
+char GlobalFileName[1024];
+char GlobalFilePath[4094];
+char GlobalQuery[4096];
+char GlobalMethod[8];
+char GlobalFileType[64];
+
 // Include Required File
 #include "lib/sds/sds.c"
 #include "lib/popenRWE/popenRWE.c"
+
 #include "config.c"
+s_conf server_conf;
+
 #include "mime-list.c"
 #include "function.c"
 #include "request.c"
@@ -60,6 +69,8 @@ int GlobalSock = -1;
 
 
 
+void handle_static_file();
+
 int main(){
 
     printf("::: %s v%s - %s :::\n\n", __ServerName, __ServerVersion, __ServerOS);
@@ -67,32 +78,42 @@ int main(){
     // Declare Socket Parameters
     int bytesRecv = -1;
     int MainSocket = -1, AcceptSocket = -1;
-    char strSock[8];
-    //int SecureSocket;
+    //char strSock[8];
+
+
+    // Cleanup Globals
+    GlobalSock = -1;
+
 
     // Request Data
-    rqpack request_data;
+    //rqpack request_data;
 
     // KGI
-    pid_t pid;
-    int kpipe[3];
-    char kbuffer[1024] = "";
+    //pid_t pid;
+    //int kpipe[3];
+    //char kbuffer[1024] = "";
 
     // Khudro Gateway Interface
-    pthread_t kgi_thread;
+    pthread_attr_t tattr;
+    pthread_t kgi_thread, static_thread;
+
+    pthread_attr_init(&tattr);
+    pthread_attr_setstacksize(&tattr, 1 * 1024);
+    pthread_attr_setdetachstate(&tattr, 1);
+
 
     // File
-    FILE *fp;
-    char *file_type = malloc(128);
-    char kgi_path_call[512] = "";
-    size_t file_size = 0;
+    //FILE *fp;
+    //char *file_type = malloc(128);
+    //char kgi_path_call[512] = "";
+    //size_t file_size = 0;
 
     // Load Default Configuration
-    s_conf server_conf;
+    //s_conf server_conf;
     server_conf = parse_config("khudro.conf", 1);
 
     if(server_conf.default_port > 0){
-        MainSocket = ListenHTTP(server_conf);
+        MainSocket = ListenHTTP();
     }
 
     if(server_conf.ssl_port > 0){
@@ -118,7 +139,8 @@ int main(){
     }
 
     // Receive Data From Client
-    bytesRecv = process_request(AcceptSocket, server_conf, &request_data);
+    //bytesRecv = process_request(AcceptSocket, server_conf, &request_data);
+    bytesRecv = process_request(AcceptSocket);
 
 
     if(bytesRecv < 0){
@@ -127,122 +149,93 @@ int main(){
         goto listen_for_client; // Repeat Process
     }
 
-    if(access(request_data.file_path, F_OK) != -1){
+    //if(access(request_data.file_path, F_OK) != -1){
+    if(access(GlobalFilePath, F_OK) != -1){
         // File Found, Now Get Some Information About File And Send It
 
-        get_mime_type(request_data.file_name, file_type); // Get MIME Type
+        //get_mime_type(request_data.file_name, file_type); // Get MIME Type
+        get_mime_type(GlobalFileName, GlobalFileType); // Get MIME Type
 
-        if(strcmp(file_type, "text/x-c") == 0){
+        if(strcmp(GlobalFileType, "text/x-c") == 0 || strcmp(GlobalFileType, "text/x-cpp") == 0){
             // Send It To KGI
 
+            //printf("\nKGI: Calling... ");
 
-
-            //map_set(kgi_data, "DEFAULT_DIR", server_conf.default_dir);
-            //map_set(kgi_data, "OBJECT_DIR", server_conf.object_dir);
-            //map_set(kgi_data, "SCRIPT_PATH", request_data.file_path);
-            //sprintf(strSock, "%d", AcceptSocket);
-            //map_set(kgi_data, "CLIENT_SOCKET", strSock);
-
-            //strcpy(kgi_arg.default_dir, server_conf.default_dir);
-            //strcpy(kgi_arg.object_dir, server_conf.object_dir);
-            //strcpy(kgi_arg.script_path, request_data.file_path);
-
-            //strcpy(kgi_arg.default_dir, "htdocs");
-            //strcpy(kgi_arg.object_dir, "objects");
-            //strcpy(kgi_arg.script_path, "htdocs/hello.c");
-
-            //kgi_arg.client_socket = AcceptSocket;
-
-
-            printf("\nKGI: Calling...");
-
-            //khudro_gateway_interface(NULL);
-            //return 0;
-
+            //strcpy(GlobalFilePath, request_data.file_path);
+            //strcpy(GlobalQuery, request_data.query);
+            //strcpy(GlobalFileType, file_type);
             GlobalSock = AcceptSocket;
-            if(pthread_create(&kgi_thread, NULL, khudro_gateway_interface, NULL)){
+            if(pthread_create(&kgi_thread, &tattr, &khudro_gateway_interface, NULL)){
                 printf("\nError: Creating Thread");
                 return 1;
             }
 
+            //pthread_detach(kgi_thread);
 
-            /*
-            setEnv("DEFAULT_DIR", server_conf.default_dir);
-            setEnv("OBJECT_DIR", server_conf.object_dir);
-            setEnv("SCRIPT_PATH", request_data.file_path);
-            sprintf(strSock, "%d", AcceptSocket);
-            setEnv("CLIENT_SOCKET", strSock);
-            printf("\nKGI: Environment Set!");
-            // SET MORE ENV!!
-            */
-            //strcpy(kgi_path_call, server_conf.kgi_path);
-            //strcat(kgi_path_call, " > yahoo.txt");
-            //system(kgi_path_call);
-
-            //printf("\nKGI: %s", kgi_path_call);
-            //printf("\nENV[0]: %s\n", environ[0]);
-/*
-            pid = popenRWE(kpipe, kgi_path_call);
-
-            sprintf(strSock, "%d", AcceptSocket);
-
-            write(kpipe[0], server_conf.default_dir, strlen(server_conf.default_dir)*sizeof(char));
-            write(kpipe[0], " | ", 3*sizeof(char));
-            write(kpipe[0], server_conf.object_dir, strlen(server_conf.object_dir)*sizeof(char));
-            write(kpipe[0], " | ", 3*sizeof(char));
-            write(kpipe[0], request_data.file_path, strlen(request_data.file_path)*sizeof(char));
-            write(kpipe[0], " | ", 3*sizeof(char));
-            write(kpipe[0], strSock, strlen(strSock)*sizeof(char));
-
-            close(kpipe[0]);
-
-            printf("\nRead [ 01 ]");
-            while(read(kpipe[1], &kbuffer, sizeof(kbuffer))){
-                printf("%s", kbuffer);
-            }
-
-            printf("\nRead [ 02 ]");
-            while(read(kpipe[2], &kbuffer, sizeof(kbuffer))){
-                printf("%s", kbuffer);
-            }
-            pcloseRWE(pid, kpipe);
-*/
-            //close(AcceptSocket);
-
-            //fpipe = popen(kgi_path_call, "r");
-            //pipenum = fileno(fpipe);
-            //fcntl(pipenum, F_SETFL, O_NONBLOCK);
-            //pclose(fpipe);
-            //pid = fork();
-            //if(pid == 0){
-            //    execle(kgi_path_call, NULL, environ);
-            //    printf("\nKGI__: %s", kgi_path_call);
-            //    return 0;
-            //}else{
-            //    printf("\nKGI--: %s", kgi_path_call);
-            //}
-
-
-
-            printf("\nKGI: Call Finished!");
+            //printf("\nKGI: Call Finished!\n");
 
             // DO NOT CLOSE SOCKET!!
         }else{
             // Handle It In Static Handler
-            fp = fopen(request_data.file_path, "rb");
-            file_size = getFileSize(fp); // Get File Size
-            send_response_header(AcceptSocket, 200, file_type, file_size); // Send Header
-            send_response_file(AcceptSocket, fp, file_size, server_conf.max_buffer); // Send File To Client
-            close(AcceptSocket); // Close Connection
-            fclose(fp); // Close File
+            //strcpy(GlobalFilePath, request_data.file_path);
+            //strcpy(GlobalQuery, request_data.query);
+            //strcpy(GlobalFileType, file_type);
+            GlobalSock = AcceptSocket;
+            if(pthread_create(&static_thread, &tattr, &handle_static_file, NULL)){
+                printf("\nError: Creating Thread");
+                return 1;
+            }
+            //pthread_detach(static_thread);
         }
+
+        //GlobalSock = -1;
+        //free(GlobalFileName);
+        //free(GlobalFilePath);
+        //free(GlobalQuery);
     }else{
         send_response_error(AcceptSocket, 404, server_conf, 0); // No Debug Print
         close(AcceptSocket); // Close Connection
+
     }
+
+    //free(request_data.file_name);
+    //free(request_data.file_path);
+    //free(request_data.query);
+    //printf("-> %u", &request_data->requested_uri);
+
+
+    //GlobalSock = -1;
+    //free(GlobalFileName);
+    //free(GlobalFilePath);
+    //free(GlobalQuery);
+
+    fflush(stdin);
+    fflush(stdout);
 
     //close(AcceptSocket); // Close Connection
     goto listen_for_client; // Repeat Process
 
     return 0;
+}
+
+void handle_static_file(){
+    FILE *fp;
+    size_t file_size;
+
+    int LocalSock = GlobalSock;
+    char LocalFilePath[4096];
+    char LocalFileType[64];
+
+    strcpy(LocalFilePath, GlobalFilePath);
+    strcpy(LocalFileType, GlobalFileType);
+
+
+    fp = fopen(LocalFilePath, "rb");
+    file_size = getFileSize(fp); // Get File Size
+    send_response_header(LocalSock, 200, LocalFileType, file_size); // Send Header
+    send_response_file(LocalSock, fp, file_size, server_conf.max_buffer); // Send File To Client
+    close(LocalSock); // Close Connection
+    fclose(fp); // Close File
+
+
 }
